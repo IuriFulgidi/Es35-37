a207ee78f801e6e2a00df05cf590b6a90d383087
iurifulgidi <iurifulgidi@hotmail.com>
2019-12-05 14:45:32 +0100

    fine

---
+++ MultiThreading/Equazione/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Equazione
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //risolvere (2*6)+(1+4)*(5-2)
+            Console.WriteLine("risoluzione ( 2 * 6 ) + ( 1 + 4 ) * ( 5 - 2 )");
+            //dichiarazione variabili
+            int ris_par1 = 0;
+            int ris_par2 = 0;
+            int ris_par3 = 0;
+            int ris_molt = 0;
+            int ris_finale;
+
+            //operazioni tra le parentesi
+            var parentesi1 = Task.Factory.StartNew(() =>
+            {
+                //attesa di un secondo per testare che effettivamente funzioni
+                Thread.Sleep(1000);
+                ris_par1 = 2 * 6;
+                Console.WriteLine($"prima parentesi : 2 * 6 = {ris_par1}");
+            });
+            var parentesi2 = Task.Factory.StartNew(() =>
+            {
+                ris_par2 = 1 + 4;
+                Console.WriteLine($"seconda  parentesi : 1 + 4 = {ris_par2}");
+            });
+            var parentesi3 = Task.Factory.StartNew(() =>
+            {
+                ris_par3 = 5 - 2;
+                Console.WriteLine($"terza  parentesi : 5 - 2 = {ris_par3}");
+            });
+
+            //moltiplicazione
+            //attesa delle due parentesi
+            Task[] tmpTaskArray = new Task[] { parentesi2, parentesi3 };
+            Task.WaitAll(tmpTaskArray);
+
+            //moltiplicazione effettiva
+            var moltiplicazione = Task.Factory.StartNew(() =>
+            {
+                ris_molt = ris_par2 * ris_par3;
+                Console.WriteLine($"moltiplicazione: {ris_par2} * { ris_par3} = {ris_molt}");
+            });
+
+            //calcolo finale
+            //List<Task> taskList = GetTaskList();
+            //attesa del completamento di tutte le task
+            Task[] taskArray = new Task[] { parentesi1, moltiplicazione };
+            Task.WaitAll(taskArray);
+
+            //risultato finale
+            var risultato = Task.Factory.StartNew(() =>
+            {
+                ris_finale = ris_molt + ris_par1;
+                Console.WriteLine($"somma: {ris_par1} + {ris_molt} = {ris_finale}");
+            });
+
+            Task.WaitAny(risultato);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MultiThreading/Maggiore/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maggiore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //stabilire il maggiore tra 3 numeri
+            int x;
+            int y;
+            int z;
+            int k;
+
+            //modalità sequanziale
+            x = 20;
+            y = 13;
+            z = 21;
+            k = Max(x, y);
+            k = Max(k, z);
+            Console.WriteLine($"in numero più grande tra {z}, {y} e {z} è {k}");
+
+            //modalità multithreading
+            var leggi_x = Task.Factory.StartNew(() =>
+            {
+                x = 20;
+            });
+            var leggi_y = Task.Factory.StartNew(() =>
+            {
+                y = 13;
+            });
+
+            //attesa delle due letture
+            Task[] tmpTaskArray = new Task[] { leggi_x, leggi_y };
+            Task.WaitAll(tmpTaskArray);
+
+            var max_xy = Task.Factory.StartNew(() =>
+            {
+                k = Max(x, y);
+            });
+            var leggi_z = Task.Factory.StartNew(() =>
+            {
+                z = 21;
+            });
+
+            //attesa 
+            Task[] tmpTaskArray1 = new Task[] { max_xy, leggi_z };
+            Task.WaitAll(tmpTaskArray);
+
+            var max_kz = Task.Factory.StartNew(() =>
+            {
+                k = Max(k, z);
+            });
+
+            Console.WriteLine($"in numero più grande tra {z}, {y} e {z} è {k}");
+
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// restituisce il più grande fra due numeri
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <returns></returns>
+        static int Max(int num1, int num2)
+        {
+            if (num1 > num2)
+                return num1;
+            else
+                return num2;
+        }
+    
+    }
+}
